10646f3f06a62c382bacd9286f1504c468e1ff81
Gray-Wanderer <andrey.mish@gmail.com>
2021-03-20 16:21:15 +0300

    Wishlist

---
+++ templates/react_static_root/src/pages/index.add16709.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{58:function(e,n,t){"use strict";t.r(n);var a=t(0),c=t.n(a);n.default=function(){return c.a.createElement("div",{style:{textAlign:"center"}},c.a.createElement("p",null,c.a.createElement("code",null,"//TODO: create main page")))}}}]);
